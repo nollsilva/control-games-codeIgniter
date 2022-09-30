854617766292922a3518c3a68ceeac4a7c1f35c1
nollsilva <nollramwislen2017@gmail.com>
2022-09-30 19:35:51 -0300

    edit de jogos pronto e lixeira de exclusao

--- application/controllers/Games.php
+++ application/controllers/Games.php
@@ -1,21 +1,21 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Games extends CI_Controller{
+class Games extends CI_Controller
+{
 
 	public function index()
 	{
 		$this->load->model("games_model");
 		$data["games"] = $this->games_model->index();
 		$data["title"] = "Games - codeigniter";
-		
-		
+
+
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/nav-top', $data);
 		$this->load->view('pages/games', $data); //esta buscando arquivo dentro da pasta views 
 		$this->load->view('templates/footer', $data);
 		$this->load->view('templates/js', $data);
-
 	}
 
 	public function new()
@@ -27,16 +27,37 @@ class Games extends CI_Controller{
 		$this->load->view('pages/form-games', $data); //esta buscando arquivo dentro da pasta views 
 		$this->load->view('templates/footer', $data);
 		$this->load->view('templates/js', $data);
-
 	}
 
 	public function store()
-	{	
+	{
 		$game = $_POST;
-		$game["user_id"] = '1'; 
+		$game["user_id"] = '1';
 		$this->load->model("games_model");
 		$this->games_model->store($game);
 		redirect("dashboard");
-
 	}
+
+	public function edit($id)
+	{
+		$this->load->model("games_model");
+		$data["game"] = $this->games_model->show($id);
+		$data["title"] = "Editar Game - codeigniter";
+
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/nav-top', $data);
+		$this->load->view('pages/form-games', $data); //esta buscando arquivo dentro da pasta views 
+		$this->load->view('templates/footer', $data);
+		$this->load->view('templates/js', $data);
+	}
+
+	public function update($id)
+	{
+		$this->load->model("games_model");
+		$game = $_POST;
+		$this->games_model->update($id, $game);
+		redirect("games");
+	}
+
 }
